0cb42d108827fd01b325cbc5db67a88261de72a0
Jonathan Hartman <j@hartman.io>
2018-09-02 19:18:31 -0700

    [carbon] Clean up the Carbon plan and tests a little

--- carbon/test/integration/default/app_test.rb
+++ carbon/test/integration/default/app_test.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 pkg_origin = ENV['HAB_ORIGIN']
-pkg_name = 'carbon'
-pkg_ver = command("ls /hab/pkgs/#{pkg_origin}/#{pkg_name}").stdout.split.last
-pkg_build = command("ls /hab/pkgs/#{pkg_origin}/#{pkg_name}/#{pkg_ver}")
-            .stdout.split.last
-pkg_path = File.join('/hab/pkgs', pkg_origin, pkg_name, pkg_ver, pkg_build)
+pkg_path = command("hab pkg path #{pkg_origin}/carbon").stdout
 pip_path = File.join(pkg_path, 'bin/pip')
 
 describe directory('/opt/graphite') do
@@ -23,14 +19,8 @@ describe command("PYTHONPATH=#{pkg_path}/lib #{pip_path} show carbon") do
 end
 
 {
-  # 'Django' => '1.5.5',
-  # 'django-tagging' => '0.3.6',
-  # 'pyparsing' => nil,
-  # 'python-memcached' => nil,
-  # 'pytz' => nil,
   'Twisted' => '13.1.0',
   'txAMQP' => nil,
-  # 'uWSGI' => nil,
   'whisper' => nil,
   'zope.interface' => nil
 }.each do |pkg, ver|
