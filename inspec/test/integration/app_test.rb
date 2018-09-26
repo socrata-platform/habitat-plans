a0393959d099cc758f5c78b1a34facfd7dd45a32
Jonathan Hartman <j@hartman.io>
2018-09-26 15:12:38 -0700

    [inspec] Fix the failing integration test
    The newline needs to be stripped off the end of the package path.

--- inspec/test/integration/app_test.rb
+++ inspec/test/integration/app_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 pkg_origin = ENV['HAB_ORIGIN']
-pkg_path = command("hab pkg path #{pkg_origin}/inspec").stdout
+pkg_path = command("hab pkg path #{pkg_origin}/inspec").stdout.strip
 
 describe command("#{pkg_path}/bin/inspec --help") do
   its(:exit_status) { should eq(0) }
